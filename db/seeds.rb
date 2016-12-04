97ba95d66bb27e510082699bf26d260510adafe6
diegous <munoa.d@gmail.com>
2016-12-04 00:25:32 -0300

    Agregué seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,96 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Priorities
+values = ['low', 'medium', 'high']
+values.each { |value| Priority.create(name: value) }
+
+# States
+values = ['pending', 'finished']
+values.each {|value| State.create(name: value)}
+
+# ============= #
+#    Lista 0    #
+# ============= #
+list = List.create(title: 'Lista 0')
+
+def task_values(list, task, i)
+  { list: list, state: State.all[i], priority: Priority.all[i], description: "#{task} task #{i}" }
+end
+
+3.times do |i|
+  SimpleTask.create    task_values(list, 'simple', i)
+  LongTask.create      task_values(list, 'long', i).merge(percentage: i*25)
+  TemporaryTask.create task_values(list, 'long', i).merge(start_date: Date.today + i, end_date: Date.tomorrow + i*2)
+end
+
+
+
+# =================== #
+#    Lista hogeraña   #
+# =================== #
+list = List.create(title: 'Tareas hogerañas')
+
+low, medium, high = Priority.all
+pending, finished = State.all
+
+# Simple Tasks
+SimpleTask.create(
+  list: list,
+  description: 'Lavar sábanas',
+  state: pending,
+  priority: low
+)
+
+SimpleTask.create(
+  list: list,
+  description: 'Cambiar foco del baño',
+  state: pending,
+  priority: high
+)
+
+SimpleTask.create(
+  list: list,
+  description: 'Regar cactus',
+  state: pending,
+  priority: low
+)
+
+
+# Long Tasks
+LongTask.create(
+  list: list,
+  description: 'Pintar cocina',
+  state: pending,
+  priority: medium,
+  percentage: 25
+)
+
+LongTask.create(
+  list: list,
+  description: 'Arreglar pérdida de la canilla del baño',
+  state: finished,
+  priority: low,
+  percentage: 80
+)
+
+
+# Temporary Tasks
+TemporaryTask.create(
+  list: list,
+  description: 'Pagar la luz',
+  state: pending,
+  priority: high,
+  start_date: Date.today - 4,
+  end_date: Date.tomorrow + 10
+)
+
+TemporaryTask.create(
+  list: list,
+  description: 'Terminar de comer el guiso de la heladera antes de que se pudra',
+  state: pending,
+  priority: high,
+  start_date: Date.today - 2,
+  end_date: Date.tomorrow + 2
+)
